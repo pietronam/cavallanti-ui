1845e284a36a5b2e5d0da59406be36be3531920a
nam <pietronam.cavallanti@gmail.com>
2025-01-13 17:27:24 +0100

    init app

---
+++ documentation-app/src/app/app.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Button } from '@cavallanti-ui/button';
+
+
+export function App() {
+  return (
+    <div>
+      <Button/>
+    </div>
+  );
+}
+
+export default App;
--- packages/button/src/lib/button.tsx
+++ packages/button/src/lib/button.tsx
@@ -1,5 +1,3 @@
-import styles from './button.module.css';
-
 export function Button() {
   return (
     <button>BUTTON</button>
