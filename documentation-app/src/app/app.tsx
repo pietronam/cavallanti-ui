008880c9c0d738dea3f7f1cec77d1fa0b32db463
nam <pietronam.cavallanti@gmail.com>
2025-01-14 15:30:02 +0100

    feat: implement grid

--- documentation-app/src/app/app.tsx
+++ documentation-app/src/app/app.tsx
@@ -1,12 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Button } from '@cavallanti-ui/button';
+import { Grid } from '@cavallanti-ui/grid';
 
 
 export function App() {
   return (
     <div>
       <Button label='BOTTONE' color='white' bgColor='green' onClick={() => alert("HELP")}/>
-      
+      <Grid columns={3} gap={10}>
+        <span>hi</span>
+        <span>hi</span>
+        <span>hi</span>
+        <span>hi</span>
+        <span>hi</span>
+      </Grid>
     </div>
   );
 }
