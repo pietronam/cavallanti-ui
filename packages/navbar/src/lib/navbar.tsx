0df8037b8b6e499637a16aa0632692629b3d4838
nam <pietronam.cavallanti@gmail.com>
2025-01-14 14:28:16 +0100

    init navbar

---
+++ packages/navbar/src/lib/navbar.tsx
@@ -0,0 +1,11 @@
+import styles from './navbar.module.css';
+
+export function Navbar() {
+  return (
+    <div className={styles['container']}>
+      <h1>NAVBAR TEXT</h1>
+    </div>
+  );
+}
+
+export default Navbar;
